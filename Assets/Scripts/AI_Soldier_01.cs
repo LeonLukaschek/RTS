58cc5a9e38a6be9744689cb95e4e9c3b05da3b58
Leon Lukaschek <leon.lukaschek@gmail.com>
2015-12-25 16:44:32 +0100

    Init commit

---
+++ Assets/Scripts/AI_Soldier_01.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Soldier_01 : MonoBehaviour
+{
+    [Header("Patrol")]
+    public Transform[] points;
+
+    private int desPoint = 0;
+
+    NavMeshAgent agent;
+
+    bool patrol = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            patrol = false;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            patrol = true;
+            GoToNextPoint();
+        }
+
+        if (agent.remainingDistance < 0.5f && patrol)
+            GoToNextPoint();
+
+    }
+
+    void GoToNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[desPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        desPoint = (desPoint + 1) % points.Length;
+    }
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    //Used to define an area to spawn the soldiers in
+    [Header("Spawn Area")]
+    public Vector3 maxSpawnArea;
+    public Vector3 minSpawnArea;
+
+    [Space (10)]
+    [Header("Object References")]
+    //The soldier prefab
+    public GameObject soldierPref;
+    //Used for organisation
+    public GameObject soldierHolder;
+
+    [Space(10)]
+    [Header("Other")]
+    //count of soldiers
+    public int soldierCount;
+    //Time between soldier spawns
+    public int timeBetweenSpawn;
+    //spawned soldier cont
+    private int spawnedSoldiers = 0;
+    //Time used for spawning
+    private float curTime;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        SpawnSoldier(soldierCount);
+    }
+    //Spawns a soldiers
+    void SpawnSoldier(int count)
+    {
+        curTime += Time.deltaTime;
+        if (spawnedSoldiers < count && curTime > timeBetweenSpawn)
+        {
+            Debug.Log("Spawning a soldier");
+
+            GameObject soldier = Instantiate(soldierPref, new Vector3(Random.Range(minSpawnArea.x, maxSpawnArea.x), Random.Range(minSpawnArea.y, maxSpawnArea.y), Random.Range(minSpawnArea.z, maxSpawnArea.z)), Quaternion.identity) as GameObject;
+            //Add soldier to soldierHolder GameObject
+            soldier.transform.SetParent(soldierHolder.transform);
+            spawnedSoldiers++;
+            curTime = 0;
+        }
+    }
+}
